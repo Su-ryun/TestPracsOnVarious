8f16ce0b76c3a4af72c8e46e83699496ba56a57c
Su-ryun <dlee67@msudenver.edu>
2019-10-03 00:23:51 -0600

    Moar stuff.

--- some-app/src/app/app.component.spec.ts
+++ some-app/src/app/app.component.spec.ts
@@ -2,15 +2,19 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { SomeClass } from './example-classes/SomeClass';
 
+// Describe is used to house all the tests those are related to
+// whatever I am trying to test.
 describe('AppComponent', () => {
   
+  let appComponent: AppComponent = null;
   var someVar: SomeClass = null;
 
   // beforeEach() is used for setting up objects and stuff,
   // essentially, before the actual tests run.
   beforeEach(async(() => {
+    appComponent = new AppComponent();
     someVar = new SomeClass();
-    
+
     // The configureTestingModule method takes in an object that has to use the TestModuleMetadata interface.
     TestBed.configureTestingModule(
       // Whatever that is being tossed inside the configureTestingModule
@@ -22,6 +26,10 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+  it('My first component test.', () => {
+    expect(appComponent.title).toBe('some-app');
+  })
 
   it('Our first Jasmine test', () => {
     expect(true).toBe(true);
@@ -40,4 +48,11 @@ describe('AppComponent', () => {
     expect(someVar.getSomeField()).not.toBe("SomeField");
     expect(someVar.getSomeField()).toBe("NotJustAnyField");
   })
+
+  // "tests commonly have a teardown part ..."
+  // "... to make sure instances of variables get detroyed."
+  // - Testing Angular Applications.
+  afterEach(() => {
+    someVar = null;
+  });
 });
