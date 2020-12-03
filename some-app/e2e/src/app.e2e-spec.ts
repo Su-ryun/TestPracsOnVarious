35883475da69e08a1afc6e31d1dd64e67eb32b25
dlee67 <keyboardPuncher96@gmail.com>
2020-12-02 21:27:09 -0700

    Wait, then what's the reason why element API can't interact with non-Angular pages?

--- some-app/e2e/src/app.e2e-spec.ts
+++ some-app/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,7 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { By } from 'selenium-webdriver';
+import { time } from 'console';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +10,21 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('some-app app is running!');
+  // https://stackoverflow.com/questions/20927652/how-to-use-protractor-on-non-angularjs-website
+  // If I am interacting with a non-Angular website, I can use the browser.driver to directily interact with the browser driver.
+  it('Login', () => {
+    browser.driver.get('https://www.google.com/');
+
+    browser.driver.sleep(5000);
+    browser.driver.findElement(By.id('gb_70')).click();
+    browser.driver.findElement(By.id('identifierId')).sendKeys('SOO SECRET');
+    browser.driver.findElement(By.className('VfPpkd-RLmnJb')).click();
+
+    browser.driver.sleep(5000)
+    browser.driver.findElement(By.id('username')).sendKeys('EVER SECRET');
+    browser.driver.findElement(By.id('password')).sendKeys('DARKEST SECRET');
+    browser.driver.findElement(By.className('form-button')).click();
+    browser.driver.sleep(5000);
   });
 
   afterEach(async () => {
